aed7b7977ebe71aa371cdd13dccab643a0cfb761
Sany <ripiuk@ua.fm>
2017-09-25 13:16:58 +0300

    with args

--- files_size.py
+++ files_size.py
@@ -4,7 +4,7 @@ import os
 import argparse
 
 
-def get_sorted_file_size(the_path, number, max_first):
+def get_sorted_file_size(the_path, number, min_first):
     result = {}
     for root, dirs, files in os.walk(the_path):
         for fn in files:
@@ -12,24 +12,25 @@ def get_sorted_file_size(the_path, number, max_first):
             size = os.stat(path).st_size
             result[path] = size
     size_len = max(len(str(val)) for val in result.values())
-    result = sorted(result.items(), key=lambda x: x[1], reverse=max_first)
+    number_len = len(str(number))
+    result = sorted(result.items(), key=lambda x: x[1], reverse=not min_first)
+    # additional information
     print(f'Path: {the_path}')
     print(f'Number of files: {len(result)}')
-    print(f'{"b":^{size_len}} | {"mb":^7} | path')
-    # for file_path, size in result:
-    #     print(f'{size:{size_len}} | {size * 0.000001:7.1f} | {file_path}')
+    # header
+    print(f'{"№":>{number_len}} | {"b":^{size_len}} | {"mb":^3} | path')
+    # content
     for i in range(number):
-        print(f'{result[i][1]:{size_len}} | {result[i][1] * 0.000001:7.1f} | {result[i][0]}')
+        print(f'{i+1:{number_len}} | {result[i][1]:{size_len}} | {result[i][1] * 0.000001:2.1f} | {result[i][0]}')
 
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser(description='Find recursively files with max or min size')
-    # parser.add_argument('path', type=str, help='path of parent dir', default='/home')
-    # parser.add_argument('number', type=str, help='how much files will be shown', default=5)
-    # parser.add_argument('max_first', type=str, help='max or min size first (1 or 0)', default=1)
-    #
-    # args = parser.parse_args()
-    # print(args.accumulate(args.integers))
-    path = '/home/sany/PycharmProjects'
-    number = 5
-    max_first = 1
-    get_sorted_file_size(path, number, max_first)
+
+    parser = argparse.ArgumentParser(description='Finds recursively files in directory with max or min size')
+    parser.add_argument('-p', dest='path', metavar='PATH', help='the path to parent dir', action='store',
+                        default=os.getcwd())
+    parser.add_argument('-n', type=int, dest='number', metavar='NUMBER', help='how much files will be shown',
+                        action='store', default=5)
+    parser.add_argument('-m', '--min', help='sorted by min size', action='store_true')
+
+    args = parser.parse_args()
+    get_sorted_file_size(the_path=args.path, number=args.number, min_first=args.min)
