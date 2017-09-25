b6e9fd51b7cf8c1793c32afa3a7320df5487c5a6
Sany <ripiuk@ua.fm>
2017-09-25 13:52:27 +0300

    catched some exceptions

--- files_size.py
+++ files_size.py
@@ -5,23 +5,40 @@ import argparse
 
 
 def get_sorted_file_size(the_path, number, min_first):
+
+    # get result
     result = {}
     for root, dirs, files in os.walk(the_path):
         for fn in files:
             path = os.path.join(root, fn)
-            size = os.stat(path).st_size
+            try:
+                size = os.stat(path).st_size
+            except FileNotFoundError:
+                print('No such file or directory:', path)
+                return
             result[path] = size
-    size_len = max(len(str(val)) for val in result.values())
-    number_len = len(str(number))
     result = sorted(result.items(), key=lambda x: x[1], reverse=not min_first)
+
+    # set table size
+    size_len = len(str(result[0][1])) if not min_first else len(str(result[number-1][1]))
+    number_len = len(str(number))
+    mb_len = size_len - 6 if size_len - 6 > 0 else 1
+
     # additional information
     print(f'Path: {the_path}')
     print(f'Number of files: {len(result)}')
+
     # header
-    print(f'{"№":>{number_len}} | {"b":^{size_len}} | {"mb":^3} | path')
+    print(f'{"№":>{number_len}} | {"B":^{size_len}} | {"MB":^{mb_len+2}} | Path')
+
+    # check if number args > number of files
+    if number > len(result):
+        number = len(result)
+
     # content
     for i in range(number):
-        print(f'{i+1:{number_len}} | {result[i][1]:{size_len}} | {result[i][1] * 0.000001:2.1f} | {result[i][0]}')
+        print(f'{i+1:{number_len}} | {result[i][1]:{size_len}} | '
+              f'{result[i][1] * 0.000001:{mb_len+2}.1f} | {result[i][0]}')
 
 if __name__ == '__main__':
 
